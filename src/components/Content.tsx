b0f12ae2a95ada13404ff69d9ca6aad7f3f7c3ec
icaPR <polosnramos@hotmail.com>
2022-10-04 05:44:13 -0300

    create: component

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,57 @@
-export function Content() {
-  // Complete aqui
+import { useEffect, useState } from "react";
+import { api } from "../services/api";
+import { MovieCard } from "./MovieCard";
+
+interface GenreResponseProps {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+}
+interface TypeGenre {
+  genroSideBar: GenreResponseProps;
+}
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+export function Content({ genroSideBar }: TypeGenre) {
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+
+  useEffect(() => {
+    api
+      .get<MovieProps[]>(`movies/?Genre_id=${genroSideBar.id}`)
+      .then((response) => {
+        setMovies(response.data);
+      });
+  }, [genroSideBar]);
+
+  return (
+    <div className="container">
+      <header>
+        <span className="category">
+          Categoria:<span> {genroSideBar.title}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,61 @@
+import { useEffect, useState } from "react";
+import { api } from "../services/api";
+import { Button } from "./Button";
+import { Content } from "./Content";
+
 export function SideBar() {
-  // Complete aqui
+  interface GenreResponseProps {
+    id: number;
+    name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+    title: string;
+  }
+  interface MovieProps {
+    imdbID: string;
+    Title: string;
+    Poster: string;
+    Ratings: Array<{
+      Source: string;
+      Value: string;
+    }>;
+    Runtime: string;
+  }
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>(
+    {} as GenreResponseProps
+  );
+  useEffect(() => {
+    console.log("aquui");
+    api.get<GenreResponseProps[]>("genres").then((response) => {
+      setGenres(response.data);
+      setSelectedGenre(response.data[0]);
+    });
+  }, []);
+
+  function handleClickButton(genre: GenreResponseProps) {
+    setSelectedGenre(genre);
+  }
+  return (
+    <div style={{ display: "flex", flexDirection: "row" }}>
+      <nav className="sidebar">
+        <span>
+          Watch<p>Me</p>
+        </span>
+
+        <div className="buttons-container">
+          {genres.map((genre) => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre)}
+              selected={selectedGenre.id === genre.id}
+            />
+          ))}
+        </div>
+      </nav>
+      <Content genroSideBar={selectedGenre} />;
+    </div>
+  );
 }
